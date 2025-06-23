1b4cfb3b3ec791289366d4cd6e9408075fabc115
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-06-23 15:32:17 +0200

    Solved tasks of Quiz for Day1 and Day2

--- src/question.js
+++ src/question.js
@@ -1,7 +1,20 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+    shuffleChoices() {
+        let i = this.choices.lenght;
+        let j = 0;
+        let temp = 0;
+        for (let i = this.choices.length - 1; i >= 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = this.choices[i];
+            this.choices[i] = this.choices[j];
+            this.choices[j] = temp;
+        }
 
-    // 2. shuffleChoices()
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,69 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
 
-    // 4. shuffleQuestions()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions() {
+        let j = 0;
+        let temp = 0;
+        for (let i = this.questions.length - 1; i >= 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+        }
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer) {
+        if (this.questions[this.currentQuestionIndex].answer === answer) {
+            this.correctAnswers++;
+        }
+    }
+
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        }
+        return true;
+    }
+
+    filterQuestionsByDifficulty(difficulty) {
+        if (difficulty >= 1 && difficulty <= 3) {
+            this.questions = this.questions.filter(function (question) {
+                return question.difficulty === difficulty;
+            })
+        }
+    }
+
+    averageDifficulty() {
+        const sum = this.questions.reduce(function (acc, question) {
+            return acc + question.difficulty
+        }, 0);
+
+        return sum/this.questions.length;
+    }
 }
+
+const q1 = new Question("1*2",[2, 3, 4, 6], 2, 2)
+q1.shuffleChoices();
+
+const q2 = new Question("4+2",[7, 8, 4, 6], 6, 2)
+q2.shuffleChoices();
+
+const game = new Qiuz([q1, q2], 2, 1)
+
+
+
+console.log(q.choices)
